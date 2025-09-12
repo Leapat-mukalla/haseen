78dd52b8f56ea0b66ead2ab99a1d991042fcd3c3
Marwanbz <kaka.mer1998@gmail.com>
2025-09-12 23:02:32 +0300

    feat: create language switcher &   add links   constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,52 @@
+export const links = [
+  {
+    title: "الرئيسية",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "من نحن؟",
+    url: "/#about",
+  },
+  {
+    title: "المشاريع",
+    url: "http://leapat.org/projects",
+  },
+  {
+    title: "المدونة",
+    url: "http://leapat.org/blog",
+  },
+  {
+    title: "تطبيقاتنا",
+    url: "#",
+    hasDropdown: true,
+    dropdownItems: [
+      {
+        title: "حصين",
+        url: "/",
+        description: "أداة فحص تسريب البيانات",
+      },
+      // {
+      //   title: "تطبيق آخر",
+      //   url: "/app-two",
+      //   description: "وصف التطبيق الثاني",
+      // },
+    ],
+  },
+  {
+    id: 6,
+    title: "اتصل بنا",
+    url: "/#contact",
+  },
+];
+
+export const languages = [
+  {
+    code: "ar",
+    name: "العربية",
+  },
+  {
+    code: "en",
+    name: "English",
+  },
+];
